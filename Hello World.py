7b7251ac62e365dc54c559e9763a0c709972cc76
GigaSlayer <gigaslay3r@gmail.com>
2016-09-30 14:54:48 -0400

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,10 @@
+# This program says Hello and asks for my name.
+
+print('Hello World!')
+print('What is your name?') # ask for name
+myName = input()
+print('it is good to meet you, ' + myName)
+
+print(myName + ' what is your age?') # ask for age
+myAge = input()
+print ('You will be ' + str(int(myAge) + 1) + ' in a year.')
