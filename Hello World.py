bfcc253111842b5aab61946dd8144de0cc3a615d
GigaSlayer <gigaslay3r@gmail.com>
2016-09-30 14:56:14 -0400

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -4,7 +4,7 @@ print('Hello World!')
 print('What is your name?') # ask for name
 myName = input()
 print('it is good to meet you, ' + myName)
-
+# I want to add a function here that formats the String myName to have a Capitol letter first.
 print(myName + ' what is your age?') # ask for age
 myAge = input()
 print ('You will be ' + str(int(myAge) + 1) + ' in a year.')
